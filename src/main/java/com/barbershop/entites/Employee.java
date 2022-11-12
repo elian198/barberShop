4169fc0d61b6047f518190b2f89d5a23e197caab
Elian <elianpareja87@hotmail.com>
2022-11-12 16:47:28 -0300

    Create Employee Entities

---
+++ src/main/java/com/barbershop/entites/Employee.java
@@ -0,0 +1,80 @@
+package com.barbershop.entites;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
+    private String first_name;
+
+    @Column(name = "LAST_NAME")
+    private String last_name;
+
+    @Column(name = "EMAIL" ,unique = true)
+    private String email;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @OneToMany(mappedBy = "employee")
+    private Set<Customer> customers = new HashSet<>();
+
+    public Employee() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Set<Customer> customers) {
+        this.customers = customers;
+    }
+}
+
