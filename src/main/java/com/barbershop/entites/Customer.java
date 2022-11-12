df6f462337ec9b1202d1c541b24c66c09537eae8
Elian <elianpareja87@hotmail.com>
2022-11-12 15:57:22 -0300

    Create Entity Customer I miss the relationShip

---
+++ src/main/java/com/barbershop/entites/Customer.java
@@ -0,0 +1,96 @@
+package com.barbershop.entites;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
+    private String first_name;
+
+    @Column(name = "LAST_NAME")
+    private String last_name;
+
+    @Column(name = "EMAIL", unique = true)
+    private String email;
+
+    @Column(name = "PHONE", unique = true)
+    private Integer phone;
+
+    @Column(name = "BIRTH_NAME")
+    private Date birth_name;
+
+   // @OneToMany(mappedBy = "appointment")
+   //private Set<String> listAppointments = new HashSet<>();
+
+
+    public Customer() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirth_name() {
+        return birth_name;
+    }
+
+    public void setBirth_name(Date birth_name) {
+        this.birth_name = birth_name;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone=" + phone +
+                ", birth_name=" + birth_name +
+                '}';
+    }
+}
