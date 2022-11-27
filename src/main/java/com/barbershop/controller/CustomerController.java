0d083433b8f096edc6431dd959a9dacd118c5a44
Elian <elianpareja87@hotmail.com>
2022-11-27 12:25:17 -0300

    Document created

--- src/main/java/com/barbershop/controller/CustomerController.java
+++ src/main/java/com/barbershop/controller/CustomerController.java
@@ -4,6 +4,8 @@ package com.barbershop.controller;
 import com.barbershop.entites.Appointment;
 import com.barbershop.entites.Customer;
 import com.barbershop.service.impl.CustomerServiceImpl;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,8 @@ public class CustomerController {
     }
 
     @PostMapping("/customer")
+    @ApiOperation("Crea un nuevo cliente")
+    @ApiParam("Customer")
     public ResponseEntity<?> save(@RequestBody Customer customer){
         if(customer == null){
             return ResponseEntity.badRequest().body("No se puede enviar campos vacios");
@@ -30,6 +34,8 @@ public class CustomerController {
         return ResponseEntity.ok("Cliente creado");
     }
     @PutMapping("/customer")
+    @ApiOperation("Actualiza un cliente")
+    @ApiParam("Customer")
     public ResponseEntity<?> update(@RequestBody Customer customer){
 
         customerService.update(customer);
@@ -43,6 +49,7 @@ public class CustomerController {
         return ResponseEntity.ok("cliente eliminado");
     }
     @GetMapping("/customer")
+    @ApiOperation("Muestra la lista de clientes")
     public List<Customer> findAll(){
         return customerService.findAll();
     }
@@ -53,6 +60,8 @@ public class CustomerController {
         return ResponseEntity.ok(  customerService.findByEmail(email));
     }
     @PostMapping("/customer/addAppointment/{id}")
+    @ApiOperation("Agrega turnos apartir del id de un cliente")
+    @ApiParam("id, Appoinment")
     public ResponseEntity<String> addAppointment(@PathVariable Long id , @RequestBody Appointment appointment) throws FileNotFoundException {
 
         if(customerService.findById(id) == null){
@@ -62,6 +71,8 @@ public class CustomerController {
         return ResponseEntity.ok("TURNO " + appointment.getId() + " AGREGADO CORRECTAMENTE");
     }
     @DeleteMapping("/customer/deleteAppointment/{id}/{idAppointment}")
+    @ApiOperation("Eliminar un turno de un cliente")
+    @ApiParam("id, idAppointment")
     public  ResponseEntity<String> deleteAppointment(@PathVariable Long id, @PathVariable Long idAppointment) throws FileNotFoundException {
         if(customerService.findById(id) == null){
             return ResponseEntity.badRequest().body("No existe el id");
